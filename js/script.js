d793d8b0e7fd60455beb89ad40bd9537485ec7a2
rdelva <roseldelva@gmail.com>
2020-09-01 07:59:38 -0400

    added focus() to the name input

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//When the page first loads, the first text field should be in focus by default.
+
+const name = document.getElementById('name');
+name.focus();
+
+
+//when you click on the other option from the job role the other textbox should appear
