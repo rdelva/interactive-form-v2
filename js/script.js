d083b6dcf2b624cde4972522b867757592260ff7
rdelva <roseldelva@gmail.com>
2020-09-04 13:33:54 -0400

    set up createOption()

--- js/script.js
+++ js/script.js
@@ -23,33 +23,77 @@ title.addEventListener('click', (e) => {
 //check and see if seelct theme is selected
 // if remained as default. set color to Please select a T-shirt theme
 
-const design = document.getElementById('design');
-const color = document.getElementById('color');
-const colorOptions = color.getElementsByTagName('option');
-const parent  = colorOptions[0].parentNode;
 
-
-
-if(design.value  == 'Select Theme') {
+const createDropDownColorOption = () => {
     //create option called "Please select a T-shirt theme"
-    const colorOption = document.createElement('option');
-    colorOption.innerHTML = 'Please select a T-shirt theme';
 
-    // add colorOption before the first item in the drop down menu.
-        //set the colorOption value to the color drop down menu
-    parent.insertBefore(colorOption, colorOptions[0]);
-    color.value = colorOptions[0].value;
+    const color = document.getElementById('color');    
+    const colorOptions = color.getElementsByTagName('option'); //grabs the colorOptions list for later usage
+    const parent  = colorOptions[0].parentNode;
 
-    // select the color Options that have the value attribute and assign it to html
-    const colorOptionsList = color.querySelectorAll('[value]');
+    const option = document.createElement('option');
+    option.innerHTML = 'Please select a T-shirt theme';
   
-    //Remove the colors after the first option 
-    for(let i = 0; i < colorOptionsList.length ; i++ ) {
-        parent.removeChild(colorOptionsList[i]);
-    }  
-    
-}  else {
-    // put colorOptionList back into the color 
-        //make item selectable
+      // add colorOption before the first item in the drop down menu.
+          //set the colorOption value to the color drop down menu
+      parent.insertBefore(option, colorOptions[0]);
+      color.value = colorOptions[0].value;
+  
+      // select the color Options that have the value attribute and assign it to html
+      const colorOptionsList = color.querySelectorAll('[value]');
+
+     
+      // tShirtColors(colorOptions);
+      //tShirtColorHidden(colorOptions, parent);
+
 }
 
+/*
+const removeColorList = (colorList, parent) => {
+   
+
+    console.log(colorList.length);
+    if(design.value  == 'Select Theme') {      
+
+        //Remove the colors after the first option 
+        for(let i = 0; i < colorList.length; i++ ) {
+            parent.removeChild(colorList[i]);
+        }         
+    }
+
+}*/
+
+const tShirtColors = (colorChoices) =>{
+
+    for(let i = 0; i < colorChoices.length; i++){
+        console.log(colorChoices[i]);
+    } 
+
+}
+
+
+/*design.addEventListener('click', (e) => {
+    const color = document.getElementById('color');
+    const colorOptions = color.getElementsByTagName('option');
+    const parent  = colorOptions[0].parentNode;
+
+    console.log(design.value);
+
+    if(design.value  == 'Select Theme') {
+       // tShirtColorHidden();     
+        
+    } else if (design.value == '') {
+       /* // put colorOptionList back into the color 
+            //make item selectable
+            for(let i = 0; i < colorOptionsList.length; i++){
+                parent.appendChild(colorOptionsList[i]);
+            } 
+
+           console.log(); 
+    }
+
+});
+*/
+
+createDropDownColorOption();
+//tShirtColorHidden();
