556e083fa5e3ea20673c5629ef1cea8813477ad7
rdelva <roseldelva@gmail.com>
2021-01-25 21:36:42 -0500

    removed createOption removeColorList tShirtColors and design Choices..plan to rewrite

--- js/script.js
+++ js/script.js
@@ -41,111 +41,6 @@ const tShirtSelection = () => {
 
 }
 
-//check and see if seelct theme is selected
-// if remained as default. set color to Please select a T-shirt theme
-
-
-const createOption = () => {
-    //create option called "Please select a T-shirt theme"
-
-    const color = document.getElementById('color');    
-    const colorOptions = color.getElementsByTagName('option'); //grabs the colorOptions list for later usage
-   
-    const parent  = colorOptions[0].parentNode;
-
-    const option = document.createElement('option');
-    option.innerHTML = 'Please select a T-shirt theme';
-    
-    // add colorOption before the first item in the drop down menu.          
-      parent.insertBefore(option, colorOptions[0]);
-      color.value = colorOptions[0].value;  //set the Color drop down menu to Default
-
-    //return  colorOptions;    
-}
-
-
-const removeColorList = () => {
-  
-    //removes the color choices in drop down menu
-    const themes = color.querySelectorAll('[value]');     
-    const parent = themes[0].parentNode;
-    for(let i = 0; i < themes.length; i++ ) {
-            parent.removeChild(themes[i]);
-    } 
-    
-    return themes;
-}
-
-const tShirtColors = (shirtColors) =>{
-    // seperates the tshirtColors into two seperate lists based on shirt design
-    const jsPunsList = [];
-    const heartJSList = [];
-
-    for(let i = 0; i < shirtColors.length; i++){
-      
-        if(shirtColors[i].innerHTML.includes('JS Puns shirt only')){
-            jsPunsList.push(shirtColors[i]);
-        } else {
-            heartJSList.push(shirtColors[i]);
-
-        }        
-    } 
-    // send the two arrays into designChoices() in order to be appended based on choice
-   designChoices(jsPunsList, heartJSList);
-}
-
-
-const designChoices = (jsPuns, heartJS) => {
-
-
-    const design = document.querySelector('#design');
- 
-
-    if( jsPuns != undefined || heartJS != undefined){
-
-        
-        design.addEventListener('change', (e) => {
-            const selectedTheme = e.target.value;
-            const color = document.getElementById('color');
-            const option  = color.querySelectorAll('option'); 
-            const parent = option[0].parentNode; //uses the first option to get the parent
-            
-
-            if(selectedTheme == 'js puns'){
-                if(option.length > 1 ){
-                    removeColorList();
-                    for(let i = 0; i < heartJS.length; i++){
-                        parent.appendChild(jsPuns[i]);
-                    }
-                } else {
-                    
-                    for(let i = 0; i < jsPuns.length; i++){
-                        parent.appendChild(jsPuns[i]);
-                    }
-                }              
-              
-            } else if( selectedTheme == 'heart js'){
-                if(option.length > 1 ){
-                    removeColorList();
-                    for(let i = 0; i < heartJS.length; i++){
-                        parent.appendChild(heartJS[i]);
-                    }
-                } else {
-                    for(let i = 0; i < heartJS.length; i++){
-                        parent.appendChild(heartJS[i]);
-                    }
-                }             
-            } else {
-                shirtLegend.style.color = 'red';
-                console.log(shirtLegend);
-                removeColorList();
-
-            }
-
-        });
-    }
-}
-
 const registerActivities = () => {
     let price = document.createElement('div');
     price.id ="price"
@@ -260,13 +155,6 @@ const paymentInfo = () => {
 
     });
 }
-// setting up errorMessages for the forms 
-
-/*const createErrorMessage = () => {
-    const div  = document.createElement('div');
-    div.style.color = "red";
-
-}*/
 
 
 const errorMessages = () => {
@@ -504,13 +392,11 @@ const submitAndVerify =  () => {
 
 }
 tShirtSelection();
-//createErrorMessage();
+
 errorMessages();
 
-createOption();
-const shirtColors = removeColorList();
-tShirtColors(shirtColors);
-designChoices();
+
+
 registerActivities();
 paymentInfo();
 realTimeChecking();
